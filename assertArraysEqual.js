f7107fa1c830a3cfa69e039fb827b7ec4ebf178e
Eric Dokter <edokter11@gmail.com>
2022-01-28 12:15:25 -0700

    fixed eqArrays function and added assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,7 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//eqArrays function that takes in 2 arrays and returns true or false
 const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
     return false
@@ -20,9 +19,13 @@ const eqArrays = function (array1, array2) {
   return true
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+const assertArraysEqual = function (array1, array2) {
+  let result = eqArrays(array1, array2)
+  if (result === true) {
+    console.log("These arrays match!")
+  } else{
+    console.log("These arrays do not match!")
+  }
+}
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertArraysEqual([1, 2, 3], [1, 3, 3])
