1fe49fc4a4044d28d5bf9184586cd514c84db061
Eric Dokter <edokter11@gmail.com>
2022-01-31 14:39:23 -0700

    letterPositions function created

--- eqArrays.js
+++ eqArrays.js
@@ -25,4 +25,4 @@ console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([], []), true)
