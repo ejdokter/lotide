35bfd1c7863e5971c0d06741b6f250771d3854ba
Eric Dokter <edokter11@gmail.com>
2022-02-15 10:39:51 -0700

    removed eqArrays

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,5 +1,4 @@
 const middle = require('../middle')
 const assertArraysEqual = require('../assertArraysEqual')
-const eqArrays = require('../eqArrays')
 
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
