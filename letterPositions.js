5a43202324ca92fe4e13ec4b652d9e10c9ef0e31
Eric Dokter <edokter11@gmail.com>
2022-02-18 11:34:28 -0700

    refactored letterPositions and added test

--- letterPositions.js
+++ letterPositions.js
@@ -1,30 +1,7 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false
-    }
-  }
-  return true
-}
-
-const assertArraysEqual = function (array1, array2) {
-  let result = eqArrays(array1, array2)
-  if (result === true) {
-    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`)
-  } else{
-    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`)
-  }
-}
-
 const letterPositions = function (sentence) {
   const results = {}
 
   for (let i = 0; i < sentence.length; i++) {
-    // console.log(sentence.indexOf(letter))
     if (sentence[i] === " ") {
       continue;
     } if (!results[sentence[i]]) {
@@ -35,5 +12,5 @@ const letterPositions = function (sentence) {
   return results
 }
 
-console.log(letterPositions("lighthouse in the house"))
-console.log(assertArraysEqual(letterPositions("hello").e, [1]))
+module.exports = letterPositions
+console.log(letterPositions("lighthouse in the house").h)
